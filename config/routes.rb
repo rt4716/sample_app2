5c7f119cf17a33bc65c397b749e4b11987b4ede8
Ryuichi Takashima <ryuichi.takashima@gmail.com>
2023-04-15 15:06:19 +0000

    add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'for/bar'
-  get 'for/baz'
   root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 end
