46cb779115b989288a98ff91b22ff146712d4abb
rt4716 <ryuichi.takashima@gmail.com>
2023-05-08 13:33:24 +0000

    list 7.3

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   get  "/about", to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
+
+  # below "resources: users" is equal with
+  # get "/users", to: "users#index"
+  # get "users/1", to: "users#show"
+  # ...
+  resources :users
+
 end
