e77a39f28d1aa6bb56c8d07ab60cedc27dc904eb
rt4716 <ryuichi.takashima@gmail.com>
2023-05-16 12:52:29 +0000

    list 10.19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  # before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -41,7 +42,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-        # beforeフィルタ
+    # beforeフィルタ
 
     # ログイン済みユーザーかどうか確認
     def logged_in_user
