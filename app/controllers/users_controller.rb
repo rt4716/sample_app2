ed77849f986dd8a0a4be74d32367fd464ba38a0b
rt4716 <ryuichi.takashima@gmail.com>
2023-05-18 13:04:24 +0000

    list 10.29

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -56,6 +55,6 @@ class UsersController < ApplicationController
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless @user == current_user
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 end
