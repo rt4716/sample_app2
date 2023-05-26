a463c41b3e926cba2339499d3b8fb36282a139a5
rt4716 <ryuichi.takashima@gmail.com>
2023-05-26 09:50:08 +0000

    list 13.67

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -76,13 +76,13 @@ class ImageUploadTest < MicropostsInterface
 
   test "should have a file input field for images" do
     get root_path
-    assert_select 'input[type= （コードを書き込む）]'
+    assert_select 'input[type=file]'
   end
 
   test "should be able to attach an image" do
     cont = "This micropost really ties the room together."
     img  = fixture_file_upload('kitten.jpg', 'image/jpeg')
     post microposts_path, params: { micropost: { content: cont, image: img } }
-    assert （コードを書き込む）.image.attached?
+    assert assigns(:micropost).image.attached?
   end
 end
