82009393b5a06031d0f7f2c20d34901275edcefa
rt4716 <ryuichi.takashima@gmail.com>
2023-05-26 09:59:27 +0000

    list 13.75

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,10 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to :
+  
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
+
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
