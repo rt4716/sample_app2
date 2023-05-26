07e0c00e2fc289a0e29f48c46dbef62467272f76
rt4716 <ryuichi.takashima@gmail.com>
2023-05-26 11:16:36 +0000

    list 13.75 fix

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,6 @@ class Micropost < ApplicationRecord
     attachable.variant :display, resize_to_limit: [500, 500]
   end
 
-  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
