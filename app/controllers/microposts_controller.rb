a687ecd810769128fe6fdd7081509e6dcca9329c
rt4716 <ryuichi.takashima@gmail.com>
2023-05-26 09:23:05 +0000

    list 13.56

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -14,6 +14,13 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    if request.referrer.nil?
+      redirect_to root_url, status: :see_other
+    else
+      redirect_to request.referrer, status: :see_other
+    end
   end
 
   private
