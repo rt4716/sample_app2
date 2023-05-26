173602ddb7c6e759ac29ff9ff0f8367db980267b
rt4716 <ryuichi.takashima@gmail.com>
2023-05-26 12:39:32 +0000

    list 14.52

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,7 +90,7 @@ class User < ApplicationRecord
   def feed
     part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
     Micropost.left_outer_joins(user: :followers)
-             .where(part_of_feed, { id: id })
+             .where(part_of_feed, { id: id }).distinct
              .includes(:user, image_attachment: :blob)
   end
 
